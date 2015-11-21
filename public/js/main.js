b82ac883b71a3fba21326b7b3b88fa8124d672ba
Anna Rankin <anna@annarankin.com>
2015-11-21 12:10:39 -0500

    basic phaser canvas set up

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
 console.log("HELLO I AM HERE (main.js)");
+
+// window.onload = function() {
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
+
+
+function preload () {
+  game.load.image('person', 'img/person.png')
+}
+
+function create () {
+  var dude = game.add.image(0,0,'person');
+}
+
+function update () {
+
+}
+// };
