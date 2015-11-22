ace0c6b8962b7910faee5cbda1c9365566f0b6df
Anna Rankin <anna@annarankin.com>
2015-11-22 13:37:59 -0500

    adding a lot of char data to json file

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,28 +13,31 @@ var characters;
 var setup = {
   preload: function() {
     // load any needed images/data
+    // Generic person placeholder
     game.load.image('person', 'assets/images/person.png');
-
+    // JUST RECTANGLES
     game.load.image('cat1', 'assets/images/cat1.png');
     game.load.image('cat2', 'assets/images/cat2.png');
     game.load.image('cat3', 'assets/images/cat3.png');
     game.load.image('cat4', 'assets/images/cat4.png');
     game.load.image('cat5', 'assets/images/cat5.png');
     game.load.image('cat6', 'assets/images/cat6.png');
-    
+    // Indicators/cursors/etc
     game.load.spritesheet('wand', 'assets/images/magic_girl_wand.png', 128, 128, 1);
     game.load.image('indicator', 'assets/images/indicator.png');
-    
+    // Structural images
     game.load.image('modal_bg', 'assets/images/modal_bg.png');
     game.load.image('tweet_bg', 'assets/images/tweet_bg.png');
     game.load.image('left-menu', 'assets/images/left_menu.png');
     game.load.image('tweet_box', 'assets/images/structure/tweet_box.png');
-    
+    // Static lady images
     game.load.image('blanche', 'assets/images/gg1/GG1.png');
     game.load.image('dorothy', 'assets/images/gg2/GG2.png');
-    
+    game.load.image('rose', 'assets/images/gg3/gg3.png');
+    // Lady sprites <3
     game.load.spritesheet('blanche_sprite', 'assets/images/gg1/gg1_sprite.png', 559, 625, 2);
     game.load.spritesheet('dorothy_sprite', 'assets/images/gg2/gg2_sprite.png', 343, 660, 2);
+    game.load.spritesheet('rose_sprite', 'assets/images/gg3/gg3_sprite.png', 409, 647, 2);
     game.load.spritesheet('person_sprite', 'assets/images/person_sprite.png', 237, 519, 2);
     //sfx
     game.load.audio('fairy_wand', 'assets/audio/BrainJam_Fairy_Wand.wav');
@@ -60,16 +63,16 @@ var setup = {
     });
     // determine who's depressed & set that up
     var randIdx = Math.floor(Math.random() * charData.length);
-    game.gameData.charData[randIdx].depressed = true; 
-    game.state.start('stageOne');   
+    game.gameData.charData[randIdx].depressed = true;
+    game.state.start('stageOne');
   }
 }
 
 var stageOne = {
   create: function() {
     // Play stage one music!
-    var stageOneMusic = game.add.audio('stage_one');
-    stageOneMusic.loopFull();
+    game.gameData.stageOneMusic = game.add.audio('stage_one');
+    game.gameData.stageOneMusic.loopFull();
     // create all characters!
 
     characters = game.add.group();
@@ -89,10 +92,10 @@ var stageOne = {
       // Give them the data!!
       // currentCharacter.name = character.name;
       currentCharacter.data = character;
-    //   // Set up character to take input
+      //   // Set up character to take input
       currentCharacter.inputEnabled = true;
       currentCharacter.events.onInputDown.add(showTweets, this);
-    //   // add an indicator to character that will be hidden if they're clicked on
+      //   // add an indicator to character that will be hidden if they're clicked on
       currentCharacter.indicator = game.add.image(95 + 200 * idx, game.world.centerY - 150, 'indicator')
       currentCharacter.indicator.alpha = 0;
       characters.add(currentCharacter);
@@ -100,7 +103,7 @@ var stageOne = {
     // start every char off with one tweet
     characters.forEach(function(character) {
 
-      var nextTweetDelay = Math.round(2 + Math.random() * 10) * 1000;
+      var nextTweetDelay = Math.round(2 + Math.random() * 5) * 1000;
       // call the addTweet function again on a delay
       game.time.events.add(nextTweetDelay, addTweet, this, character);
     });
@@ -111,7 +114,7 @@ var stageOne = {
 }
 
 var stageTwo = {
-  preload: function() {
+  preload: function(stageOneMusic) {
     game.gameData.categories = [
       "cats",
       "puppies",
@@ -120,6 +123,7 @@ var stageTwo = {
       "whatever",
       "whatever"
     ]
+    game.gameData.stageOneMusic.fadeOut(1000);
     // game.gameData.clickCounter = 0;
   },
   create: function() {
@@ -178,7 +182,7 @@ var stageTwo = {
 
 
     // place character on screen
-    var currentCharacter = game.add.sprite(550, 300,  game.gameData.chosenCharacter.data.imgKey + '_sprite');
+    var currentCharacter = game.add.sprite(550, 300, game.gameData.chosenCharacter.data.imgKey + '_sprite');
     currentCharacter.anchor.set(0.5);
     currentCharacter.scale.setTo(0.65, 0.65);
     currentCharacter.inputEnabled = true;
@@ -206,6 +210,33 @@ var stageTwo = {
 // ---------- Function Definitions ---------- 
 // Todo - get these outta this file IT'S SO MESSY OH GAWD
 
+function animateSquare() {
+  //play music
+  // Create new category-square image
+  var square = game.add.image(game.gameData.chosenCategory.position.x + 2, game.gameData.chosenCategory.position.y + 2, 'cat1');
+
+  // tween it to the character's location
+  var squareTween = game.add.tween(square)
+  squareTween.to({
+    x: 500,
+    y: 250
+  }, 200, Phaser.Easing.Linear.None, true);
+  // once that's done: 
+  squareTween.onComplete.add(function(square) {
+    // remove square from game
+    square.destroy();
+  });
+
+  // Need a separate tween for scale <3
+  var squareScaleTween = game.add.tween(square.scale).to({
+    x: 0.1,
+    y: 0.1
+  }, 200, Phaser.Easing.Linear.None, true);
+
+  // animate particle effects on char?
+
+}
+
 function showResponse(character, depressed, likesCategory) {
   // Disable clicks on all categories
   game.gameData.gifCategories.forEach(function(gifCategory) {
@@ -215,29 +246,14 @@ function showResponse(character, depressed, likesCategory) {
     // Start shooting gifs! Shoot for 3 - 5 seconds
     var attackMusic = game.add.audio('attack');
     attackMusic.play();
-    var shootGifsTimer = game.time.events.repeat(100, 20, function() {
-    //play music
-    // Create new category-square image
-      var square = game.add.image(game.gameData.chosenCategory.position.x + 2, game.gameData.chosenCategory.position.y + 2, 'cat1');
+    var shootGifsTimer = game.time.events.repeat(100, 20, animateSquare, this);
 
-      // tween it to the character's location
-      var squareTween = game.add.tween(square)
-      squareTween.to({x: 500, y: 250}, 200, Phaser.Easing.Linear.None, true);
-      // once that's done: 
-      squareTween.onComplete.add(function(square) {
-        // remove square from game
-        square.destroy();
-      });
-
-      // Need a separate tween for scale <3
-      var squareScaleTween = game.add.tween(square.scale).to({x: 0.1, y: 0.1}, 200, Phaser.Easing.Linear.None, true);
-
-        // animate particle effects on char?
-
-    }, this);
     // After timer event completes looping, display "not depressed" fail message
     // Note to self - refactor, do this with a callback
     game.time.events.add(3000, function(attackMusic) {
+
+      //create twitter bg img
+
       console.log(character.data.tweets.unnecessary[0]);
       attackMusic.fadeOut(1000);
     }, this, attackMusic);
@@ -349,8 +365,8 @@ function showTweets(character) {
   wand.events.onInputDown.add(function(character) {
     var fairySound = game.add.sound('fairy_wand', 0.5);
     fairySound.play();
-    game.state.start('stageTwo');
-  });
+    game.state.start('stageTwo', true, false, game.gameData.stageOneMusic);
+  }, this);
 
   // Setting up modal to have "click out of meee" functionality
   modalBg.inputEnabled = true;
